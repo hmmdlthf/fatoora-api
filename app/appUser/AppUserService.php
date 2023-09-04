c75473db0ce89232f9d6c8a524fddb8ddacd34de
kevin_jorn <Althaf>
2023-09-04 12:11:26 +0530

    arrange the structure

--- app/appUser/AppUserService.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$ROOT = $_SERVER["DOCUMENT_ROOT"];
-require_once $ROOT . '/vendor/autoload.php';
-require_once $ROOT . "/app/user/UserService.php";
-
-class AppUserService extends UserService
-{
-}
